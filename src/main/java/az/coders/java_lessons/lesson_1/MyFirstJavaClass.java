2636ea6bede86e89dcbc20545df3c6c43955def1
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-22 19:38:23 +0400

    Initial commit

---
+++ src/main/java/az/coders/java_lessons/lesson_1/MyFirstJavaClass.java
@@ -0,0 +1,9 @@
+package az.coders.java_lessons.lesson_1;
+
+public class MyFirstJavaClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi Ismayil, Welcome to the Java world");
+    }
+}
