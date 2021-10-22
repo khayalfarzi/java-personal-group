b58b0405039acc4885255e4f71e940f6a4364a22
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-22 19:46:12 +0400

    lesson 1: Git learning

---
+++ src/main/java/az/coders/java_lessons/lesson_1/GitExample.java
@@ -0,0 +1,8 @@
+package az.coders.java_lessons.lesson_1;
+
+public class GitExample {
+
+    public static void main(String[] args) {
+        System.out.println(" We are learning git clone, push , commit, pull and itc");
+    }
+}
