b4b707794d35840490b86a304fbfa810366df38b
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-23 11:25:11 +0400

    lesson 2: JVM, JRE, JDK, JIT Comments, Variables, download and configuration maven

---
+++ src/main/java/az/coders/java_lessons/lesson_2/CommentsJava.java
@@ -0,0 +1,24 @@
+package az.coders.java_lessons.lesson_2;
+
+public class CommentsJava {
+
+    // This is single line comment
+
+    /*
+     * This
+     * is
+     * multi
+     * line
+     * comment
+     * ...
+     * */
+
+    /**
+     * @Author: Khayal Farziyev
+     * <p>
+     * This is javadoc comments.
+     * <p>
+     * Integer type is equal to 5
+     */
+    int a = 5;
+}
---
+++ src/main/java/az/coders/java_lessons/lesson_2/JavaOperators.java
@@ -0,0 +1,30 @@
+package az.coders.java_lessons.lesson_2;
+
+public class JavaOperators {
+
+    public static void main(String[] args) {
+        /**
+         * Primitive types:
+         *
+         * 1. boolean (true, false)
+         * 2. byte (2^8)
+         * 3. short (2^16)
+         * 4. int (2^32)
+         * 5. double (2^64)
+         * 6. float (2^32)
+         * 7. long (2^64)
+         * 8. char(2^15)
+         * */
+
+        int a = 5;
+        int b, c, d;
+        b = 6;
+        c = 7;
+        d = b + c;
+
+        System.out.println(a);  // 5
+        System.out.println(b);  // 6
+        System.out.println(c);  // 7
+        System.out.println(d);  // 13
+    }
+}
