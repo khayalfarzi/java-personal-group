91ae7a0df463bd7bc73eddf7567b6c59ec5d77e2
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-27 18:35:59 +0400

    lesson 3: warmup

---
+++ src/main/java/az/coders/java_lessons/lesson_3/OperatorsExample.java
@@ -0,0 +1,15 @@
+package az.coders.java_lessons.lesson_3;
+
+public class OperatorsExample {
+
+    public static void main(String[] args) {
+        // Technical requirements
+
+        // define two number of integer num1 and num2
+        // for ex : num1= 5; and num2 = 12;
+        // The task is :
+        // write a program to reverse numbers
+        // result must be : num1 = 12 and num2 = 5;
+
+    }
+}
