6333fc0ad23743f9001f83c0df2c6f924a6a5988
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-27 19:50:19 +0400

    lesson 3: task

---
+++ src/main/java/az/coders/java_lessons/lesson_3/ConditionalOperators.java
@@ -0,0 +1,20 @@
+package az.coders.java_lessons.lesson_3;
+
+import java.util.Scanner;
+
+public class ConditionalOperators {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the application");
+
+        System.out.print("Enter the number: ");
+
+        int num = sc.nextInt();
+
+        // if number is positive then print "positive"
+        // else if number is negative then print "negative"
+        // else if number is 0 then print "zero"
+    }
+}
