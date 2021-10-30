14a97661c518ccae8b4676ddc9ff85cf07210ee8
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-30 10:38:32 +0400

    lesson 4: warmup

---
+++ src/main/java/az/coders/java_lessons/lesson_4/FindCharacters.java
@@ -0,0 +1,13 @@
+package az.coders.java_lessons.lesson_4;
+
+public class FindCharacters {
+
+    public static void main(String[] args) {
+
+        // You have a given word: (ex) "This is the word"
+
+        // Enter the limits: 1-5
+
+        // print: "his i"
+    }
+}
