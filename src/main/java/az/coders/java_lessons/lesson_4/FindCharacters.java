aec3c16266e2718e18d03afdf84027fc419264ef
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-30 11:14:25 +0400

    lesson 4: warmup

--- src/main/java/az/coders/java_lessons/lesson_4/FindCharacters.java
+++ src/main/java/az/coders/java_lessons/lesson_4/FindCharacters.java
@@ -1,5 +1,7 @@
 package az.coders.java_lessons.lesson_4;
 
+import java.util.Scanner;
+
 public class FindCharacters {
 
     public static void main(String[] args) {
@@ -9,5 +11,26 @@ public class FindCharacters {
         // Enter the limits: 1-5
 
         // print: "his i"
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Application!");
+        System.out.println();
+
+        // User enter the word which he/she want to split
+        System.out.print("Enter word: ");
+        String word = sc.nextLine();
+
+        // User enter the limits
+        System.out.print("Enter limits (ex: n-m): ");
+        String limits = sc.next();
+
+        // Application split high and low limits
+        int bottomLimit = Integer.parseInt(limits.substring(0, 1));
+        int topLimit = Integer.parseInt(limits.substring(2, 3));
+
+        // Application print the result
+        String result = word.substring(bottomLimit, topLimit);
+        System.out.printf("The result is: %s", result);
     }
 }
--- src/main/java/az/coders/java_lessons/lesson_4/FindMax.java
+++ src/main/java/az/coders/java_lessons/lesson_4/FindMax.java
@@ -1,5 +1,7 @@
 package az.coders.java_lessons.lesson_4;
 
+import java.util.Scanner;
+
 public class FindMax {
 
     public static void main(String[] args) {
@@ -13,5 +15,20 @@ public class FindMax {
         // third = 12
 
         // The result is : 56
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to the Application!");
+
+        System.out.print("Enter first number: ");
+        int first = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int second = sc.nextInt();
+
+        System.out.print("Enter third number: ");
+        int third = sc.nextInt();
+
+        System.out.printf("The result is : %s", Math.max(Math.max(first, second), third));
     }
 }
