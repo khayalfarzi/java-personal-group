f1baf6d7a6890c126c255b12d13747a0b7f45e68
khayalfarzi <khayal.farziyev@pashabank.az>
2021-10-30 10:31:01 +0400

    lesson 4: warmup

---
+++ src/main/java/az/coders/java_lessons/lesson_4/FindMax.java
@@ -0,0 +1,17 @@
+package az.coders.java_lessons.lesson_4;
+
+public class FindMax {
+
+    public static void main(String[] args) {
+
+        // Find Maximum number between 3 different numbers
+
+        // The task is :
+        // User enter 3 number . For ex:
+        // first = 23
+        // second = 56
+        // third = 12
+
+        // The result is : 56
+    }
+}
