8d9b8d1cd439ebaa0a3e0b4498a13ab3e6e033b7
khayalfarzi <khayal.farziyev@pashabank.az>
2021-11-03 19:14:43 +0400

    lesson 5: warmup

---
+++ src/main/java/az/coders/java_lessons/lesson_5/ArrayPrintEx.java
@@ -0,0 +1,15 @@
+package az.coders.java_lessons.lesson_5;
+
+public class ArrayPrintEx {
+
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][3];
+
+        // write code for enter random values in the array
+
+        // write code for print values from array
+        // 1 2 3
+        // 4 5 6
+    }
+}
---
+++ src/main/java/az/coders/java_lessons/lesson_5/MathCalc.java
@@ -0,0 +1,19 @@
+package az.coders.java_lessons.lesson_5;
+
+public class MathCalc {
+
+    public static void main(String[] args) {
+
+        int num = 6984;
+
+        /*
+         *
+         * Print following:
+         *
+         * Minlik: 6
+         * Yuzluk: 9
+         * Onluq:  8
+         * T'klik: 4
+         * */
+    }
+}
