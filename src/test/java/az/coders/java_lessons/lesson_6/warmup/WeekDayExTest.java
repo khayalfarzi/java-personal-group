48b057240df9cac2a5de0701c19f67ed3dd37d48
khayalfarzi <xeyalferziyev9@gmail.com>
2021-11-06 18:48:11 +0300

    lesson 6: warmup, added methods and unit tests

--- src/test/java/az/coders/java_lessons/lesson_6/warmup/WeekDayExTest.java
+++ src/test/java/az/coders/java_lessons/lesson_6/warmup/WeekDayExTest.java
@@ -1,10 +1,13 @@
 package az.coders.java_lessons.lesson_6.warmup;
 
+import org.junit.Test;
+
 import static az.coders.java_lessons.lesson_6.warmup.WeekDayEx.getWeekday;
 import static org.junit.Assert.assertEquals;
 
 public class WeekDayExTest {
 
+    @Test
     public void testGetWeekday() {
         assertEquals("WEDNESDAY", getWeekday(3));
     }
