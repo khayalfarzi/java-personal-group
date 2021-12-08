41ec16c0b3099824fb551d54b102c0f5193f890d
ISKANBV <nabiyevismayil6@gmail.com>
2021-12-08 18:49:18 +0400

    metodlar dolduruldu.

--- src/main/java/az/coders/java_lessons/lesson_13/util/XArrayList.java
+++ src/main/java/az/coders/java_lessons/lesson_13/util/XArrayList.java
@@ -1,5 +1,7 @@
 package az.coders.java_lessons.lesson_13.util;
 
+import java.util.Arrays;
+
 public class XArrayList<T> implements XList<T> {
 
     private static final int capacity = 10;
@@ -22,17 +24,26 @@ public class XArrayList<T> implements XList<T> {
 
     @Override
     public T add(int index, T element) {
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        elementData[index] = element;
+        size++;
         return null;
     }
 
     @Override
     public void remove(int index) {
-
+        System.arraycopy(elementData, 0, elementData, 0, index);
+        System.arraycopy(elementData, index + 1, elementData, index, elementData.length - index - 1);
     }
 
     @Override
     public T get(int index) {
-        return (T) elementData[index];
+        for (int i = 0; i < size(); i++) {
+            if(i == index){
+                return (T) elementData[i];
+            }
+        }
+        return null;
     }
 
     @Override
@@ -42,27 +53,28 @@ public class XArrayList<T> implements XList<T> {
 
     @Override
     public int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < elementData.length; i++) {
+            if (elementData[i] != null){
+                count = i;
+            }
+        }
+        return count;
     }
 
     @Override
     public void set(int index, T element) {
-
+        elementData[index] = element;
     }
 
     @Override
     public void sort() {
-
+        Arrays.sort(elementData);
     }
 
     private void ensureCapacity(int size, Object[] elementData) {
-        if (checkSize(size, elementData)) {
+        if (size >= elementData.length)
             this.elementData = new Object[size * 2];
-            System.arraycopy(elementData, 0, elementData, 0, elementData.length);
-        }
-    }
-
-    private boolean checkSize(int size, Object[] elementData) {
-        return size >= elementData.length;
+        System.arraycopy(elementData, 0, elementData, 0, elementData.length);
     }
 }
