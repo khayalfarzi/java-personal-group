aba175356cdd3b744299fb044ef77906ba69fb4a
Khayal Farziyev <khayal.farziyev@pashabank.az>
2021-12-24 22:27:32 -0800

    lesson 19: small application

---
+++ src/main/java/az/coders/java_lessons/lesson_19/small_app/Gender.java
@@ -0,0 +1,12 @@
+package az.coders.java_lessons.lesson_19.small_app;
+
+public enum Gender {
+
+    MALE("male"), FEMALE("female");
+
+    private final String value;
+
+    Gender(String value) {
+        this.value = value;
+    }
+}
